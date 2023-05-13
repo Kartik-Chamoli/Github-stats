02b52bdceece23ddcd096c96fc6d91edf599f73b
ayush <ayush.chauhan0008@gmail.com>
2023-05-13 13:09:05 +0530

    Edit ReadMe

--- views/searchView.js
+++ views/searchView.js
@@ -1,18 +1,18 @@
-import {domElements,dynamicEle} from './base.js';
+import { domElements, dynamicEle } from './base.js';
 
 
-export function userImagesMarkup(ele,user){
+export function userImagesMarkup(ele, user) {
     let markup = `<figure class="${user.id}">
     <img class="fig" src="${user.avatar_url}" alt="User Image">
       <figcaption class="fig">${user.name}</figcaption>
   </figure>`
 
-  ele.insertAdjacentHTML('beforeend',markup);
+    ele.insertAdjacentHTML('beforeend', markup);
 }
 
 
-export function createCard(user){
-    let markup =`<div class="cardContainer ${user.id}">
+export function createCard(user) {
+    let markup = `<div class="cardContainer ${user.id}">
     <div class="left-side">
         <img src="${user.avatar_url}" alt="">
         <h2>${user.login}</h2>
@@ -40,53 +40,53 @@ export function createCard(user){
     </div>
 </div>`
 
-    domElements.userStats.insertAdjacentHTML('beforeend',markup);
+    domElements.userStats.insertAdjacentHTML('beforeend', markup);
     cardMarkup(user);
 }
 
-function cardMarkup(user){
+function cardMarkup(user) {
     let newElements = dynamicEle.cardEle(user.id);
 
-    if(user.company){
-        newElements.leftSide.insertAdjacentHTML('beforeend','        <span>Company: Hcl Technologies</span>');
+    if (user.company) {
+        newElements.leftSide.insertAdjacentHTML('beforeend', '        <span>Company: Hcl Technologies</span>');
     }
-    if(user.twitter_username){
-        newElements.socials.insertAdjacentHTML('beforeend',` <a href="https://www.twitter.com/${user.twitter_username}"><span class="iconify" data-inline="false"
+    if (user.twitter_username) {
+        newElements.socials.insertAdjacentHTML('beforeend', ` <a href="https://www.twitter.com/${user.twitter_username}"><span class="iconify" data-inline="false"
         data-icon="ant-design:twitter-circle-filled" style="color: #5700C5;"></span></a>`);
     }
-    if(user.blog){
-        newElements.socials.insertAdjacentHTML('beforeend',`
+    if (user.blog) {
+        newElements.socials.insertAdjacentHTML('beforeend', `
         <a href=${user.blog}> <span class="iconify" data-inline="false" data-icon="whh:website"
         style="color: #5700C5;"></span></a>`);
     }
-    if(user.location){
-        newElements.location.insertAdjacentHTML('beforeend',`
+    if (user.location) {
+        newElements.location.insertAdjacentHTML('beforeend', `
         <span class="loc">Location: <span class="rVal">${user.location}</span></span>
         `);
     }
-    if(user.bio){
-        newElements.rightContainer.insertAdjacentHTML('beforeend',`
+    if (user.bio) {
+        newElements.rightContainer.insertAdjacentHTML('beforeend', `
         <div class="quote">
             <span class="rVal">${user.bio}</span>
         </div>`);
     }
 }
 
-export function toggleVisibility(hideId){
-    let ele=document.querySelectorAll(`.${hideId}`); 
-    for(let i=1;i<4;i++){
+export function toggleVisibility(hideId) {
+    let ele = document.querySelectorAll(`.${hideId}`);
+    for (let i = 1; i < 4; i++) {
         ele[i].classList.toggle('hidden');
-    }    
+    }
 }
 
-export function clearDom(){
-    domElements.userStats.innerHTML="";
+export function clearDom() {
+    domElements.userStats.innerHTML = "";
 }
 
-export function getInput(){
+export function getInput() {
     return domElements.searchBox.value;
 }
 
-export function clearInput(){
+export function clearInput() {
     domElements.searchBox.value = "";
 }
